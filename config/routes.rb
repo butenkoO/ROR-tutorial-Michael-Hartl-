fabc3d04734e040be245d7848e2e40d24a51c91c
ButenkoA <andriybutenko94@gmail.com>
2021-05-25 09:02:43 +0300

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'application#home'
 
   # Example of regular route:
-    get '/' => 'application#home'
+    get 'home', to: 'application#home'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
